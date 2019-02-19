ba60f5924757bde40dbf29d42d4150ac2d8577d0
Leonardo Lurci <leonardo.lurci@nubess.com>
2019-02-19 12:45:32 +0100

    first commit

---
+++ WebApplication3/Pages/Custom/SectionA/CustomPageA.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApplication3.Pages.Standard.SectionA;
+
+namespace WebApplication3.Pages.Custom.SectionA
+{
+    public class CustomPageAModel : PageAModel
+    {
+        public override void OnGet()
+        {
+        }
+    }
+}
