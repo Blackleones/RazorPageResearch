84f9852b1fe14fcd82291eeb7a3d9ad382c16b0f
newbienewbie <itminus@hotmail.com>
2019-02-20 12:19:56 +0800

    create RazorResearch.Core project

---
+++ RazorResearch.Core/Pages/SectionA/PageA.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorResearch.Core.SectionA
+{
+    public class PageAModel : PageModel
+    {
+        public virtual void OnGet()
+        {
+        }
+    }
+}
