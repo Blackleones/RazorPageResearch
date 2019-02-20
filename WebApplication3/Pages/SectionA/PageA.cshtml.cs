edfdb25efb1eb4d7eb296ca6088149bafb127c18
newbienewbie <itminus@hotmail.com>
2019-02-20 12:20:31 +0800

    Custom the standard Page within WebAppliaction3

---
+++ WebApplication3/Pages/SectionA/PageA.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorResearch.Core.SectionA;
+
+namespace WebApplication3.Pages.SectionA
+{
+    /// <summary>
+    /// override partial
+    /// </summary>
+    public class CustomPageAModel : PageAModel
+    {
+        public override void OnGet()
+        {
+        }
+    }
+}
